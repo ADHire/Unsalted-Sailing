ea7cdcf31b587a8187d2df955d179888bf8ede40
mkkokay <marinakassel13@gmail.com>
2015-12-11 14:50:50 -0500

    Added Directive

--- js/companyLists.js
+++ js/companyLists.js
@@ -8,5 +8,19 @@ app.controller('myController', function ($scope){
 		$scope.companies.push(
 			{name: $scope.newCompany,
 			linked: $scope.newLinked});
-	}
+	};
+});
+
+app.directive('coke', function () {
+    return {
+        restrict:'E',
+        transclude:true,
+        scope: true,
+        template:'<div class="coke">' +
+            '<h3>{{company.company || "company name"}}</h3>' +
+            '<h3>{{company.linked || "company name"}}</h3>' +
+            '<div class="coke-content" ng-transclude></div>' +
+            '</div>',
+        replace:true
+    };
 });
