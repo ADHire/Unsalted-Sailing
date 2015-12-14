302d247abdfb2fa044a63a64508960ff2bd3552b
Maddy <maddy531@gmail.com>
2015-12-14 10:24:58 -0500

    Updated google signin and directive for adding a new company.

--- js/companyLists.js
+++ js/companyLists.js
@@ -10,16 +10,25 @@ app.controller('myController', function ($scope){
 			linked: $scope.newLinked});
 	}
 });
+console.log("Crappppp");
 app.directive('coke', function () {
-   return {
-       restrict:'E',
-       transclude:true,
-       scope: true,
-       template:'<div class="coke">' +
-           '<h3>{{company.company || "company name"}}</h3>' +
-           '<h3>{{company.linked || "company name"}}</h3>' +
-           '<div class="coke-content" ng-transclude></div>' +
-           '</div>',
-       replace:true
-   };
+  return {
+      restrict:'AE',
+      transclude:true,
+      scope:  true,
+      template:'<div>' +
+          '<h3>{{company.company || "company name"}}</h3>' +
+          '<h3>{{company.linked || "company name"}}</h3>' +
+          '<div class="coke-content" ng-transclude></div>' +
+          '</div>',
+    replace:true,
+    link: function (scope, el, attrs) {
+
+           // initialize the css
+           el.css({
+               'background-color': 'blue'
+           });
+
+}
+};
 });
--- js/googleSignIn.js
+++ js/googleSignIn.js
@@ -1,60 +1,13 @@
-function logout() {
-    gapi.auth.signOut();
-    location.reload();
-}
-
-function login() {
-    var myParams = {
-        'clientid' : '207327943083-9jqvp8b23d4jdubb6nd8gisrona3fenu.apps.googleusercontent.com',
-        'cookiepolicy' : 'single_host_origin',
-        'callback' : 'loginCallback',
-        'approvalprompt':'force',
-        'scope' : 'https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/plus.profile.emails.read'
-    };
-    gapi.auth.signIn(myParams);
-}
-
-function loginCallback(result) {
-    if(result['status']['signed_in']) {
-        var request = gapi.client.plus.people.get({
-            'userId': 'me'
-        });
-        request.execute(function (resp) {
-            var email = '';
-            if(resp['emails']) {
-                for(i = 0; i < resp['emails'].length; i++) {
-                    if(resp['emails'][i]['type'] == 'account') {
-                        email = resp['emails'][i]['value'];
-                    }
-                }
-            }
-
-            var googleFirstName = resp['name']['familyName'];
-            var googleLastName = resp['name']['givenName'];
-            var googleImg = resp['image']['url'];
-            var googleImgSrc = "<img src='" + resp['image']['url'] + "' />";
-            var googleURL = "URL:" + resp['url'];
-            var googleEmail = email;
-            var googleEducation = resp['organizations'][0]['name'];
-            document.getElementById("googleFirstName").innerHTML = googleFirstName;
-            document.getElementById("googleLastName").innerHTML = googleLastName;
-            document.getElementById("googleEducation").innerHTML = googleEducation;
-            document.getElementById("googleEmail").innerHTML = googleEmail;
-            document.getElementById("googleImg").src = googleImg;
-            console.log("profile");
-        });
-
-    }
-
-}
-
-function onLoadCallback() {
-    gapi.client.setApiKey('AIzaSyC7_W2SU2HwfVoEZbfVqgChNYKtoYZX6Ic');
-    gapi.client.load('plus', 'v1',function(){});
-}
-
-(function() {
-    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-    po.src = 'https://apis.google.com/js/client.js?onload=onLoadCallback';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-})();
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail());
+};
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  };
